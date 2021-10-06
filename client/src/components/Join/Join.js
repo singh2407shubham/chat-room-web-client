0e8016da177e6add36fb4602549e752cd55ec0a1
Shubham Singh <shubhamsingh@192.168.1.137>
2021-10-06 14:30:22 +0100

    fix

--- client/src/components/Join/Join.js
+++ client/src/components/Join/Join.js
@@ -10,11 +10,11 @@ const Join = () => {
     return (
         <div className="joinOuterContainer">
             <div className="joinInnerContainer">
-                <h1 className="heading">Join</h1>
-                <div><input placeholder="Name" className="joinInput" type="text" onChange={(e) => setName(e.target.value)} autoFocus /></div>
+                <h1 className="heading">CHAT ROOM</h1>
+                <div><input placeholder="Enter your name.." className="joinInput" type="text" onChange={(e) => setName(e.target.value)} autoFocus /></div>
                 <div><input placeholder="Room" className="joinInput mt-20" type="text" onChange={(e) => setRoom(e.target.value)} /></div>
                 <Link onClick = {(e) => (!name || !room) ? e.preventDefault() : null} to={`/chat?name=${name}&room=${room}`}>
-                    <button className="button mt-20" type="submit">Sign In</button>
+                    <button className="button mt-20" type="submit">Join</button>
                 </Link>
             </div>
         </div>
